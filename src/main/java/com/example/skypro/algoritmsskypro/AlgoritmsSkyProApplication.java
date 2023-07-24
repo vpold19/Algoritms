5f7bd0b3d61d306ecf83621cfa6e1f39eac8c810
vadim <vadimpoldolin19@gmail.com>
2023-07-24 17:47:28 +0200

    first commit

---
+++ src/main/java/com/example/skypro/algoritmsskypro/AlgoritmsSkyProApplication.java
@@ -0,0 +1,13 @@
+package com.example.skypro.algoritmsskypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmsSkyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoritmsSkyProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skypro/algoritmsskypro/AlgoritmsSkyProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skypro.algoritmsskypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmsSkyProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
