5d3b40ea5200cd2b131c24c7748c2d6e38c3f3c1
vadim <vadimpoldolin19@gmail.com>
2023-07-24 18:09:16 +0200

    first commit

--- src/main/java/com/example/skypro/algoritms/Objects/Main.java
+++ src/main/java/com/example/skypro/algoritms/Objects/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Brick brick = new Brick();
         brick.weight = 20;
-        System.out.println(brick.weight);
+        System.out.println(brick.weight + " Вес");
     }
 }
