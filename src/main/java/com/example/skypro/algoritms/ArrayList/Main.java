c31502178543f2a21f1cc68739a4c2345ac17d88
vadim <vadimpoldolin19@gmail.com>
2023-07-26 18:51:45 +0200

    arraysList

---
+++ src/main/java/com/example/skypro/algoritms/ArrayList/Main.java
@@ -0,0 +1,16 @@
+package com.example.skypro.algoritms.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(25);
+        list.add(10);
+        list.add(250);
+        list.add(36);
+       // list.add(8);
+        System.out.println(list);
+    }
+}
